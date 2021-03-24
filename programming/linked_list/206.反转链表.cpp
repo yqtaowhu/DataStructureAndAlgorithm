1470b622bb305aedd08201ebb6043836c306ec7f
taoyanqi <yqtao@whu.edu.cn>
2021-03-24 22:19:35 +0800

    add new

--- programming/linked_list/206.反转链表.cpp
+++ programming/linked_list/206.反转链表.cpp
@@ -13,6 +13,34 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// 以下两种方法必须熟练掌握
+class Solution {
+public:
+    // 递归方法
+    ListNode* dfs(ListNode* head, ListNode* prev=nullptr) {
+        if(!head) {
+            // 一定要注意呀
+            return prev;
+        }
+        ListNode* nxt = head->next;
+        head->next = prev;
+        return dfs(nxt, head);
+    }
+    ListNode* reverseList(ListNode* head) {
+        //return dfs(head, nullptr); // 递归方法
+        // 迭代方法
+        ListNode* prev = nullptr, *nxt;
+        while(head) {
+            nxt = head->next;
+            head->next = prev;
+            prev = head;
+            head = nxt;
+        }
+        return prev;
+    }
+};
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
