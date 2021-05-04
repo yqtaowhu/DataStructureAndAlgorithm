0aa1e66ad04f6e7430a720523b0e68364a22820b
taoyanqi <yqtao@whu.edu.cn>
2021-05-04 19:33:40 +0800

    62 add math method

--- programming/dynamit-programming/62.不同路径.cpp
+++ programming/dynamit-programming/62.不同路径.cpp
@@ -41,5 +41,17 @@ class Solution {
         return cur[m - 1];
     }
 }; 
+// 数学方法
+//从左上角到右下角的过程中，我们需要移动 m+n-2m+n−2 次，其中有 m-1m−1 次向下移动，n-1n−1 次向右移动。因此路径的总数，就等于从 m+n-2m+n−2 次移动中选择 m-1m−1 次向下移动的方案数，即组合数：
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long long ans = 1;
+        for (int x = n, y = 1; y < m; ++x, ++y) {
+            ans = ans * x / y;
+        }
+        return ans;
+    }
+};
 // @lc code=end
 
