e320978398df535af044abc95ae8805f380f9e03
taoyanqi <yqtao@whu.edu.cn>
2024-04-19 21:03:19 +0800

    add

--- programming/binary-search/35.搜索插入位置.cpp
+++ programming/binary-search/35.搜索插入位置.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   35.搜索插入位置.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taoyanqi <taoyanqi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 20:58:34 by taoyanqi          #+#    #+#             */
+/*   Updated: 2024/04/19 21:03:03 by taoyanqi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*
  * @lc app=leetcode.cn id=35 lang=cpp
  *
@@ -8,23 +20,21 @@
 class Solution {
 public:
     // 二分搜索，需要注意的是left,right的范围
-     int searchInsert(vector<int>& nums, int target) {
-        int len = nums.size();
-        // 特殊情况
-        if(len <= 0 ) return 0;
-        if(target > nums[len-1]) return len;
-
-        int left = 0, right = len - 1;
-        // 模板代码
-        while(left < right) {
-            int mid = (left + right) >> 1;
-            if(nums[mid] >= target) {
-                right = mid;
+    // 分三种情况写，然后可以在进行优化
+    int searchInsert(vector<int>& nums, int target) {
+        int i = 0, j =  nums.size() - 1;
+        while(i <= j) {
+            int mid = (i + j) / 2;
+            if(nums[mid] == target) {
+                i = mid;
+                return i;
+            } else if(nums[mid] < target) {
+                i = mid + 1;
             } else {
-                left = mid + 1;
+                j = mid - 1;
             }
         }
-        return left;
+        return i;
     }
 };
 // @lc code=end
