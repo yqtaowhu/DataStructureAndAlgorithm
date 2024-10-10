b6ba47afe74a0fa88ff38714f787f0119184f72e
taoyanqi <yqtao@whu.edu.cn>
2024-10-10 19:23:27 +0800

    revise

--- programming/math/12.整数转罗马数字.cpp
+++ programming/math/12.整数转罗马数字.cpp
@@ -9,18 +9,16 @@ class Solution {
 public:
     //由于加减的不同，所以讲IV这种看出一个新的字符，然后从大到小使用较大的字符
     string intToRoman(int num) {
-        string symbol[] = { "M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I" };
-        int value[] = { 1000,900,500,400, 100, 90,  50, 40,  10, 9,   5,  4,   1 };
-        string result;
-        // 循环symbol
-        for (int i = 0; num != 0; i++) {
-            // 重复使用symbol，由于构造新的字符，所以满足不重复使用4个的原理
-            while (num >= value[i]) {
+        vector<string> symbol = { "M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I" };
+        vector<int> value = { 1000,900,500,400, 100, 90,  50, 40,  10, 9,   5,  4,   1 };
+        string res;
+        for(int i=0; num != 0 ; i++) {
+            while(num >= value[i]) {
+                res += symbol[i];
                 num -= value[i];
-                result += symbol[i];
             }
         }
-        return result;
+        return res;
     }
 };
 // @lc code=end
