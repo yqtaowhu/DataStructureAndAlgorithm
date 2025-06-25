ee27c996cb322b6021daed9a783afa9b61861f78
taoyanqi <yqtao@whu.edu.cn>
2025-06-25 14:44:44 +0800

    hash table

--- programming/hash-table/128.最长连续序列.cpp
+++ programming/hash-table/128.最长连续序列.cpp
@@ -8,21 +8,30 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_map<int,int> m;
+        // 100,4,200,1,3,2
+        // 不断的更新边界值, 
+        // 100 l=0,r=0 -> len=1
+        // 4 l=0,r=0,len=1
+        // 200 l=0,r=0,len=1
+        // 1 l=0,r=0,len=1
+        // 3 l=0,r=1,len=2   3,4
+        // 2 l=1,r=2, len=4
         int res = 0;
-        for(int i=0;i<nums.size();i++){
-            int n = nums[i];
-            if(m.find(n) == m.end()){
-                int l = m.count(n-1) == 1 ? m[n-1] : 0;
-                int r = m.count(n+1) == 1 ? m[n+1] : 0;
-                int len = l + r + 1;
-                m[n] = len;
-                res = max(res,len);
-                m[n-l] = len;  //特别注意更新边界值
-                m[n+r] = len;
-            }
-            else
+        unordered_map<int, int> map;
+        for(auto n : nums) {
+            if(map.find(n) == map.end()) {
+                int l = map.count(n-1) == 1 ? map[n-1] : 0;
+                int r = map.count(n+1) == 1 ? map[n+1] : 0;
+                int len = r + l + 1;
+                map[n] = len;
+                map[n-l] = len;   // 特别注意
+                map[n+r] = len;
+                res = max(len, res);
+
+            } else {
+                // 出现过的元素，不会重新更新左右边界
                 continue;
+            }
         }
         return res;
     }
---
+++ programming/hash-table/49.字母异位词分组.cpp
@@ -0,0 +1,20 @@
+
+// 典型的排序+hash问题
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (string& str: strs) {
+            string key = str;
+            sort(key.begin(), key.end());
+            mp[key].emplace_back(str);
+        }
+        vector<vector<string>> ans;
+        for (auto it = mp.begin(); it != mp.end(); ++it) {
+            ans.emplace_back(it->second);
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
