14c771a1a8dc521dbb371cb478515ab1652de7df
taoyanqi <yqtao@whu.edu.cn>
2025-07-07 18:42:17 +0800

    添加hash解法

--- programming/tree/236.二叉树的最近公共祖先.cpp
+++ programming/tree/236.二叉树的最近公共祖先.cpp
@@ -28,5 +28,36 @@ public:
         return left ? left : right; 
     }
 };
+
+
+// 使用hash表存储父节点
+class Solution {
+public:
+    unordered_map<int, TreeNode*> fa;
+    unordered_map<int, bool> vis;
+    void dfs(TreeNode* root){
+        if (root->left != nullptr) {
+            fa[root->left->val] = root;
+            dfs(root->left);
+        }
+        if (root->right != nullptr) {
+            fa[root->right->val] = root;
+            dfs(root->right);
+        }
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        fa[root->val] = nullptr;
+        dfs(root);
+        while (p != nullptr) {
+            vis[p->val] = true;
+            p = fa[p->val];
+        }
+        while (q != nullptr) {
+            if (vis[q->val]) return q;
+            q = fa[q->val];
+        }
+        return nullptr;
+    }
+};
 // @lc code=end
 
