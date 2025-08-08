80b93de270897a84b36e1eda4eaa310ad6e818ee
taoyanqi <yqtao@whu.edu.cn>
2025-08-08 16:46:40 +0800

    添加回溯算法

--- programming/backtrack/77.组合.cpp
+++ programming/backtrack/77.组合.cpp
@@ -1,3 +1,5 @@
+// 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+
 class Solution {
 public:
     vector<int> tmp;
--- programming/backtrack/79.单词搜索.cpp
+++ programming/backtrack/79.单词搜索.cpp
@@ -13,11 +13,13 @@ public:
             return false;
         if(word.size() - 1 == k) return true;
         vis[i][j] = 1;
+        // 只要有一个是true就返回true
         if(dfs(board, word, vis, rows, cols, i+1, j, k+1)
             ||dfs(board, word, vis, rows, cols, i, j+1, k+1)
             ||dfs(board, word, vis, rows, cols, i-1, j, k+1)
             ||dfs(board, word, vis, rows, cols, i, j-1, k+1))
             return true;
+        // 注意回溯
         vis[i][j] = 0;
         return false;
     }
@@ -39,3 +41,47 @@ public:
 };
 // @lc code=end
 
+class Solution {
+public:
+    vector<vector<int>> dirs = {{0,1}, {0,-1}, {1, 0}, {-1, 0}};
+    vector<vector<int>> visit;
+ 
+    bool existWord(vector<vector<char>>& board, string word, int cur, int nx, int ny){
+        if(cur == word.size() - 1){
+            return true;
+        }
+
+        int row = board.size(), col = board[0].size();
+
+        visit[nx][ny] = 1;
+        for(const auto& dir : dirs){
+            // 不要修改传进来的 nx, ny
+            int x = nx + dir[0];
+            int y = ny + dir[1];
+            if(x >= 0 && x < row && y >= 0 && y < col && visit[x][y] == 0 && board[x][y] == word[cur+1]){
+                bool flag = existWord(board, word, cur+1, x, y);
+                if(flag)
+                    return true;
+            }
+        }
+        visit[nx][ny] = 0;
+
+        return false;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int row = board.size(), col = board[0].size();
+        visit.resize(row, vector<int>(col, 0));
+
+        for(int i = 0;i < row;i++){
+            for(int j = 0;j < col;j++){
+                if(board[i][j] == word[0]){
+                    if(existWord(board, word, 0, i, j))
+                        return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
