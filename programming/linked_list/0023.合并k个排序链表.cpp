083e98f77aa51712b5c2924c71f85165de736ddf
taoyanqi <yqtao@whu.edu.cn>
2025-10-21 12:50:35 +0800

    合并k个链表增加优先队列方法

--- programming/linked_list/0023.合并k个排序链表.cpp
+++ programming/linked_list/0023.合并k个排序链表.cpp
@@ -13,6 +13,47 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // 小根堆
+        auto cmp = [](const ListNode* a, const ListNode* b) {
+            return a->val > b->val; // 最小堆
+        };
+        priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> pq; 
+        for(auto head: lists) {
+            if(head) {
+                pq.push(head);
+            }
+        }
+        ListNode dummy(0);
+        ListNode *pre = & dummy;
+        while(!pq.empty()) {
+            auto node = pq.top();
+            pq.pop();
+            if(node->next) {
+                pq.push(node->next);
+            }
+            pre->next = node;
+            pre = pre->next;
+        }
+        return dummy.next;
+        
+    }
+};
+
+
 class Solution {
 public:
 	ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
