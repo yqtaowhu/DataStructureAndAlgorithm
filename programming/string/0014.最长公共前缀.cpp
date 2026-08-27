9cf94f828d91f3c9558ee6b47de8fe4599092ca9
taoyanqi <yqtao@whu.edu.cn>
2026-08-27 21:06:41 +0800

    修改简介写法，不需要求最短字符串

--- programming/string/0014.最长公共前缀.cpp
+++ programming/string/0014.最长公共前缀.cpp
@@ -12,24 +12,11 @@
 
 class Solution {
 public:
-    // 需要正确使用substr(0, len)
     string longestCommonPrefix(vector<string>& strs) {
-        if(strs.empty()) return "";
-        int min_len = INT_MAX;
-        for(const auto& str : strs) {
-            int size = str.size();
-            min_len = min(size, min_len);
-        }
-        // 只需要比较第i位字符是不是相同
-        for(int i=0; i < min_len; i++) {
-            char s = strs[0][i];
-            for(int j=1; j < strs.size(); j++) {
-                if(strs[j][i] != s) {
+        for (int i = 0; i < strs[0].size(); i++)
+            for (int j = 1; j < strs.size(); j++)
+                if (i == strs[j].size() || strs[j][i] != strs[0][i])
                     return strs[0].substr(0, i);
-                }
-            }
-        }
-        return strs[0].substr(0, min_len);
-        
+        return strs[0];
     }
 };
